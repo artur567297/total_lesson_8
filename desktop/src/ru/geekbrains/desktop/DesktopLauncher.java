cc54830b96b6f9083cbc99bd3d377cea07ed3a4e
Артур <artur567297@gmail.com>
2019-02-11 18:16:03 +0300

    ferst

---
+++ desktop/src/ru/geekbrains/desktop/DesktopLauncher.java
@@ -0,0 +1,19 @@
+package ru.geekbrains.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+import ru.geekbrains.Star2DGame;
+import ru.geekbrains.StarGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		System.setProperty("user.name","Public");
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		float aspect = 3f/4f;
+		config.width = 400;
+		config.height = (int) (config.width/aspect);
+		config.resizable = false;
+		new LwjglApplication(new Star2DGame(), config);
+	}
+}
